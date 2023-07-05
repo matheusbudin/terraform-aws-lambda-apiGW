05c545f2adb301b4162ee2d7988ba9e658df0b51
Budin <matheusbudin@hotmail.com>
2023-07-05 15:21:06 -0300

    Update main.tf to trigger lambda from ApiGW

--- main.tf
+++ main.tf
@@ -45,9 +45,76 @@ EOF
   }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_role_attachment"{
-  role = aws_iam_role.lambda_role.name
+resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-#API gateway setting up
+resource "aws_api_gateway_rest_api" "example_api" {
+  name        = "example-api"
+  description = "Example API"
+}
+
+resource "aws_api_gateway_resource" "example_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "example_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  resource_id   = aws_api_gateway_resource.example_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# resource "aws_lambda_permission" "api_gateway_permission" {
+#   statement_id  = "AllowAPIGatewayInvoke" -> "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.hello_world_lambda.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_api_gateway_rest_api.example_api.execution_arn}/*/*/*" #added-> arn:aws:execute-api:
+# }
+
+resource "aws_api_gateway_integration" "example_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example_api.id
+  resource_id             = aws_api_gateway_resource.example_resource.id
+  http_method             = aws_api_gateway_method.example_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn    = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_method.http_method}${aws_api_gateway_resource.example_resource.path}"
+}
+
+
+resource "aws_api_gateway_deployment" "example_deployment" {
+  depends_on    = [aws_api_gateway_method.example_method, aws_api_gateway_integration.example_integration] #added aws_api_gateway_method.example_method
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  stage_name    = "dev"
+
+  lifecycle {                                                           #added lifecycle
+    create_before_destroy = true
+  }
+  
+}
+
+
+#added stage:
+resource "aws_api_gateway_stage" "example_stage" {
+  deployment_id = aws_api_gateway_deployment.example_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  stage_name    = "hml"
+}
+
+
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.example_deployment.invoke_url
+}
